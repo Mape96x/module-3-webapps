b7498ef4621705e82e998794892b68aa2c20c3f3
Marek Pentzek <marekpj96@gmail.com>
2023-11-10 12:00:22 +0100

    changes to solutions

--- coding-task-2-passwordToggle/script.js
+++ coding-task-2-passwordToggle/script.js
@@ -3,14 +3,10 @@ const inp = document.querySelector("input");
 
 btn.addEventListener("click", function () {
   if (inp.type === "text") {
-    inp.setAttribute("type", "password");
-  } else if (inp.type === "password") {
-    inp.setAttribute("type", "text");
-  }
-
-  if (inp.type === "text") {
+    inp.type = "password";
     btn.textContent = "Hide Password";
   } else if (inp.type === "password") {
+    inp.type = "text";
     btn.textContent = "Show Password";
   }
 });
